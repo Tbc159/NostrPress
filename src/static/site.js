6a118c19462375299ce6e8df96589c4cde709e82
thewrlck <thewrlck@proton.me>
2026-03-19 12:57:23 +0100

    prevent links to open new pages if not needed and update icon

--- src/static/site.js
+++ src/static/site.js
@@ -2,7 +2,7 @@
   const links = document.querySelectorAll("a");
   links.forEach((link) => {
     const href = link.getAttribute("href") || "";
-    if (href.startsWith("http")) {
+    if (href.startsWith("http") && new URL(href).hostname !== window.location.hostname) {
       link.setAttribute("target", "_blank");
       link.setAttribute("rel", "noopener noreferrer");
     }
